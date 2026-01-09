70b3509547648f96c119d751582cc041ce9a0e23
Can <101044119+can2334@users.noreply.github.com>
2026-01-09 21:12:46 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
-  poweredByHeader: false,
   reactStrictMode: true,
+  // Sitenin Next.js ile yapıldığını gizler (küçük bir güvenlik önlemi)
+  poweredByHeader: false, 
 
   images: {
-    deviceSizes: [640, 750, 828, 1080, 1200],
-    imageSizes: [16, 32, 48, 64, 96],
+    // Resimlerin patlamaması için gerekli olan Sanity ve Unsplash izinleri
     remotePatterns: [
       {
         protocol: 'https',
@@ -19,44 +19,6 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
     ],
-  },
-
-  async headers() {
-    return [
-      // 🔓 SANITY STUDIO (SERBEST)
-      {
-        source: '/studio/:path*',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: `
-              default-src * data: blob:;
-              script-src * 'unsafe-inline' 'unsafe-eval';
-              style-src * 'unsafe-inline';
-              img-src * data: blob:;
-              connect-src *;
-              frame-ancestors *;
-            `.replace(/\s{2,}/g, ' ').trim(),
-          },
-        ],
-      },
-
-      // 🔒 NORMAL SITE (GÜVENLİ)
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: `
-              default-src 'self';
-              img-src 'self' https://cdn.sanity.io https://images.unsplash.com;
-              script-src 'self';
-              style-src 'self' 'unsafe-inline';
-            `.replace(/\s{2,}/g, ' ').trim(),
-          },
-        ],
-      },
-    ]
   },
 };
 
